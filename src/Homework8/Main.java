e66711e5dd929d33fc5e7cc04f584a2a12a9c81e
annarassomahina <pupkinppppp@gmail.com>
2022-08-07 21:26:28 +0400

    I've just made a new homework project

--- src/Homework8/Main.java
+++ src/Homework8/Main.java
@@ -1,5 +1,17 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName + "";
+        System.out.println("ФИО сотрудника — " + fullName);
+        // Задание 2
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase(Locale.ROOT));
+        // Задание 3
+        String fulName = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника — " + fulName.replace('ё', 'e') );
     }
 }
